d4a1b6db6a5207e10f02b8645ffa4dd9709843be
Cristina <2023_cristina.val@iticbcn.cat>
2024-11-13 19:53:57 +0100

    ✨ POST
    POST

--- Activitat_8/main.py
+++ Activitat_8/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    name: str
+    surname: str
+    password: str
+    email: str
+    sex: str
+    age: int | None = None
+
 
 
 @app.get("/")
 async def root():
     return {"message": "Mètode GET"}
+
+
+@app.post("/create-user/")
+async def create_user(user: User):
+    return user
--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 ## GET - POSTMAN
 
 <img src="./Activitat_8/readme_img/get_postman.png"/>
+
+
+## POST - SWAGGER 
+
+<img src="./Activitat_8/readme_img/post1.png"/>
+<img src="./Activitat_8/readme_img/post2.png"/>
