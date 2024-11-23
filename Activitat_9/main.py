7f9cc844f202c3b394786b046e542ac705c434a1
Cristina <2023_cristina.val@iticbcn.cat>
2024-11-23 22:01:54 +0100

    ✅ Conexio PostgreSQL amb la consulta read_users (v2 - canvi nom fitxer readUsers)
    Conexio PostgreSQL amb la consulta read_users (v2 - canvi nom fitxer readUsers)

--- Activitat_9/main.py
+++ Activitat_9/main.py
@@ -1,6 +1,6 @@
 import connection
 import user_sch
-import users
+import readUsers
 from fastapi import FastAPI
 from typing import List
 
@@ -11,4 +11,4 @@ app = FastAPI()
 @app.get ("/users", response_model=List[dict])
 async def read_users():
     db_connection = connection.connection_db()
-    return user_sch.users_schema(users.read(db_connection))
+    return user_sch.users_schema(readUsers.read(db_connection))
